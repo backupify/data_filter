6fbbd8737b1a3e280501fb672dd8232e418d46a1
Josh Bodah <jb3689@yahoo.com>
2015-09-18 10:27:54 -0400

    try to fix coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
+require 'coveralls'
+Coveralls.wear!
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'data_filter'
 
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'minitest/pride'
-
-require 'coveralls'
-Coveralls.wear!
